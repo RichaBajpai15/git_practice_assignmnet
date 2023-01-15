9ad153f271b0c33ce9eef57ffec5e7a8b5c52cc4
Your Name <jha.chakresh2001@gmail.com>
2023-01-15 23:25:49 +0530

    done

--- prime.js
+++ prime.js
@@ -1,12 +1,12 @@
 let N=14;
 let count=0;
-for(i=0; i<N; i++){
+for(let i=1; i<=N; i++){
     if(N%i==0){
         count++
     }
-    if(count==2){
+ }
+if(count==2){
         console.log("yes")
     }else{
         console.log("no")
     }
-}
